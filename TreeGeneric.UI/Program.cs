baab64a28adfce978403c74c0d311e07f1270a4f
Bünyamin Macit <macitbunyamin@gmail.com>
2019-01-29 14:42:47 +0300

    first version

---
+++ TreeGeneric.UI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TreeGeneric.BussinessLogic;
+using TreeGeneric.Data;
+
+namespace TreeGeneric.UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Autofac =Ioc(inversion-of -control)provider'ıdır.
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ApplicationDbContext>().As<ApplicationDbContext>();
+
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            
+
+            builder.RegisterType<RegionService>().As<IRegionService>();
+            builder.RegisterType<PlantingService>().As<IPlantingService>();
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
